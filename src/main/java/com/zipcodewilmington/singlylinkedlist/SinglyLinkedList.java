20922f852c9b72e28da7e1290430eadef99067de
deanOfWalls <deanofwalls@gmail.com>
2023-04-05 17:14:07 -0400

    checkpointing after adding tests

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,5 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,107 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList<String> list;
+    @Before
+    public void before(){
+        list = new SinglyLinkedList<String>();
+    }
+    @Test
+    public void testAdd() {
+        String expected = "test";
+        list.add(expected);
+        Assert.assertTrue(list.contains(expected));
+    }
+
+    @Test
+    public void testRemove() {
+        String expected = "test";
+        list.add(expected);
+        list.remove(expected);
+        Assert.assertTrue(list.remove(expected));
+    }
+
+    @Test
+    public void testContains() {
+        String expected = "test";
+        list.add(expected);
+        Assert.assertTrue(list.contains(expected));
+    }
+
+    @Test
+    public void testFind1() {
+        int expected = 1;
+        String val = "target";
+        list.add("");
+        list.add(val);
+
+        int actual = list.find(val);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFind2() {
+        int expected = -1;
+        String val = "target";
+        list.add("");
+        list.add("");
+
+        int actual = list.find(val);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSize() {
+        int expected = 2;
+        list.add("");
+        list.add("");
+        list.add("");
+        int actual = list.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGet() {
+        String expected = "target";
+
+        list.add("");
+        list.add(expected);
+        int actual = list.get(1).data;
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCopy() {
+        list.add("");
+        list.add("1");
+        list.add("2");
+
+        SinglyLinkedList<String> newList = list.copy();
+
+        Assert.assertNotEquals(list.toString(),newList.toString());
+        for (int i = 0; i < list.size(); i++) {
+            Assert.assertEquals(list.get(i).data, newList.get(i).data);
+
+        }
+    }
+
+    @Test
+    public void testSort() {
+        list.add("0");
+        list.add("1");
+        list.add("2");
+
+        list.sort();
+        Assert.assertEquals(list.get(0).data,"0");
+        Assert.assertEquals(list.get(0).data,"1");
+        Assert.assertEquals(list.get(0).data, "2");
+    }
 }
